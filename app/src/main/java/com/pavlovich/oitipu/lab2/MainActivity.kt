9015e91c4cfe3edf6a092a6e6e0713388b5a3555
Pavlovich Nikolai <nipvvlun07@mail.ru>
2020-12-21 23:21:54 +0300

    input

---
+++ app/src/main/java/com/pavlovich/oitipu/lab2/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.pavlovich.oitipu.lab2
+
+import android.Manifest
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.pavlovich.oitipu.lab2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val saveImagePermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            if (result)
+                binding.drawingView.saveImage()
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.drawingView.apply {
+            initializePen()
+            initializeEraser()
+            setBackgroundColor(ContextCompat.getColor(context, R.color.colorWhite))
+            penSize = 10f
+            setPenColor(ContextCompat.getColor(context, R.color.colorPrimaryDark))
+        }
+
+        binding.ivSettings.setOnClickListener {
+            val dialog = SettingsBottomSheetDialogFragment.newInstance(
+                binding.drawingView.penSize.toInt(),
+                binding.drawingView.eraserSize.toInt(),
+                binding.drawingView.getPenColor(),
+                binding.drawingView.canvasColor
+            )
+
+            dialog.apply {
+                show(supportFragmentManager, "SettingsDialog")
+
+                eraserSeekBarCallback = {
+                    binding.drawingView.eraserSize = it.toFloat()
+                }
+
+                penSeekBarCallback = {
+                    binding.drawingView.penSize = it.toFloat()
+                }
+
+                penColorCallback = {
+                    binding.drawingView.setPenColor(it)
+                }
+
+                lineCallback = {
+                    binding.drawingView.isRectangleMode = false
+                    binding.drawingView.isRoundMode = false
+                    dialog.dismiss()
+                }
+
+                rectangleCallback = {
+                    binding.drawingView.isRectangleMode = true
+                    binding.drawingView.isRoundMode = false
+                    dialog.dismiss()
+                }
+
+                circleCallback = {
+                    binding.drawingView.isRectangleMode = false
+                    binding.drawingView.isRoundMode = true
+                    dialog.dismiss()
+                }
+
+                backgroundColorCallback = {
+                    binding.drawingView.changeBackground(it)
+                }
+
+                onSaveClick = {
+                    saveImagePermission.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+
+            }
+
+        }
+
+
+    }
+}
